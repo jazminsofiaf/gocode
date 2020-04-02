cae7f08750cb96da1bd3e04ad4b72d76e6aaf6d7
JazminF <Jazmina@JazsMacBookPro.local>
2020-04-02 15:30:44 -0300

    check service payload

--- bc/bcmessage/bcmessage_test.go
+++ bc/bcmessage/bcmessage_test.go
@@ -7,8 +7,20 @@ import (
 	"testing"
 )
 
+const LENGTH = 105
+const VERSION_END = 4
+const SERVICE_END  = 12
 
-func TestSendRequest(t *testing.T) {
+
+const NODE_NETWORK = 1
+const NODE_BLOOM = 4
+const NODE_WITNESS = 8
+const NODE_NETWORK_LIMITED = 1024
+
+
+
+
+func TestPayloadWithCurrentDate(t *testing.T) {
 	var payload = getPayloadWithCurrentDate()
 
 	byteArray := []byte{0, 1}
@@ -16,8 +28,8 @@ func TestSendRequest(t *testing.T) {
 		t.Errorf("wrong type payload. expected '%T', but was: '%T'", payload, byteArray)
 	}
 
-	if( len(payload) != 105){
-		t.Errorf("wrong type payload. expected '%v', but was: '%v'",  len(payload), 105)
+	if( len(payload) != LENGTH){
+		t.Errorf("wrong type payload. expected '%v', but was: '%v'",  len(payload), LENGTH)
 	}
 
 	binaryNum := []byte{0x7F, 0x11, 0x01, 0x00}
@@ -25,11 +37,22 @@ func TestSendRequest(t *testing.T) {
 	fmt.Printf("decimal num %v = %v  in binary little indian\n", num, binaryNum)
 
 	expectedBitcoinVersion := 70015
-	binaryBitcoinVersion := payload[:4]
+	binaryBitcoinVersion := payload[:VERSION_END]
 	bitcoinVersion := int(binary.LittleEndian.Uint32(binaryBitcoinVersion))
 	if(expectedBitcoinVersion != bitcoinVersion ){
 		t.Errorf("wrong version. expected '%v', but was: '%v (binary little indian = %v)'",
 			expectedBitcoinVersion, bitcoinVersion, binaryBitcoinVersion)
+	}
+
+	fmt.Println(int64(binary.LittleEndian.Uint64([]byte{13,4,0,0,0,0,0,0})))
+
+
+	expectedServices := int64(NODE_NETWORK | NODE_BLOOM | NODE_WITNESS | NODE_NETWORK_LIMITED)
+	binaryService := payload[VERSION_END:SERVICE_END]
+	service := int64(binary.LittleEndian.Uint64(binaryService))
+	if(expectedServices != service ){
+		t.Errorf("wrong version. expected '%v', but was: '%v (binary little indian = %v)'",
+			expectedServices, service, binaryService)
 	}
 
 }
