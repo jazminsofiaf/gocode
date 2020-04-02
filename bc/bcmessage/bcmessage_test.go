c480a1dcc0345ed0bce99f66ed2feed2b1ed1f86
JazminF <Jazmina@JazsMacBookPro.local>
2020-04-02 17:49:55 -0300

    check timestamp payload

--- bc/bcmessage/bcmessage_test.go
+++ bc/bcmessage/bcmessage_test.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+	"time"
 )
 
 const LENGTH = 105
 const VERSION_END = 4
 const SERVICE_END  = 12
+const DATA_END = 20
 
 
 const NODE_NETWORK = 1
@@ -21,38 +23,64 @@ const NODE_NETWORK_LIMITED = 1024
 
 
 func TestPayloadWithCurrentDate(t *testing.T) {
+
 	var payload = getPayloadWithCurrentDate()
 
+	fmt.Println("----- Type")
 	byteArray := []byte{0, 1}
 	if( reflect.TypeOf(payload) != reflect.TypeOf(byteArray)){
-		t.Errorf("wrong type payload. expected '%T', but was: '%T'", payload, byteArray)
+		t.Errorf("Wrong type payload. expected '%T', but was: '%T'", payload, byteArray)
+	}else {
+		fmt.Println("-- OK")
 	}
 
+	fmt.Println("----- Size")
 	if( len(payload) != LENGTH){
-		t.Errorf("wrong type payload. expected '%v', but was: '%v'",  len(payload), LENGTH)
+		t.Errorf("Wrong type payload. expected '%v', but was: '%v'",  len(payload), LENGTH)
+	}else {
+		fmt.Println("-- OK")
 	}
 
+	fmt.Println("----- Version")
 	binaryNum := []byte{0x7F, 0x11, 0x01, 0x00}
 	num := binary.LittleEndian.Uint32(binaryNum)
 	fmt.Printf("decimal num %v = %v  in binary little indian\n", num, binaryNum)
-
 	expectedBitcoinVersion := 70015
 	binaryBitcoinVersion := payload[:VERSION_END]
 	bitcoinVersion := int(binary.LittleEndian.Uint32(binaryBitcoinVersion))
 	if(expectedBitcoinVersion != bitcoinVersion ){
-		t.Errorf("wrong version. expected '%v', but was: '%v (binary little indian = %v)'",
+		t.Errorf("Wrong version. expected '%v', but was: '%v (binary little indian = %v)'",
 			expectedBitcoinVersion, bitcoinVersion, binaryBitcoinVersion)
+	}else {
+		fmt.Println("-- OK")
 	}
 
+	fmt.Println("---- Services")
 	fmt.Println(int64(binary.LittleEndian.Uint64([]byte{13,4,0,0,0,0,0,0})))
-
 
 	expectedServices := int64(NODE_NETWORK | NODE_BLOOM | NODE_WITNESS | NODE_NETWORK_LIMITED)
 	binaryService := payload[VERSION_END:SERVICE_END]
 	service := int64(binary.LittleEndian.Uint64(binaryService))
 	if(expectedServices != service ){
-		t.Errorf("wrong version. expected '%v', but was: '%v (binary little indian = %v)'",
+		t.Errorf("Wrong services. expected '%v', but was: '%v (binary little indian = %v)'",
 			expectedServices, service, binaryService)
+	}else {
+		fmt.Println("-- OK")
 	}
+
+	fmt.Println("---- Date")
+	binaryDate := payload[SERVICE_END:DATA_END]
+	unixTime := int64(binary.LittleEndian.Uint64(binaryDate))
+	date := time.Unix(unixTime,0)
+	now := time.Now()
+	d := 60 * time.Minute
+	if(date.Truncate(d) !=  now.Truncate(d) ){
+		t.Errorf("Wrong timestamp. expected '%v', but was: '%v'", now, date)
+	}else {
+		fmt.Println("-- OK")
+	}
+
+
+
 
 }
